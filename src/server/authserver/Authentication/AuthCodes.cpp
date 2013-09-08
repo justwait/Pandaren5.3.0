5a9d2fd4ef5e61533b8ee4317e3431b7bb4ed1b3
RaknarAtlantis <raknaratlantis@gmail.com>
2013-09-08 12:12:42 +0200

    small typo

--- src/server/authserver/Authentication/AuthCodes.cpp
+++ src/server/authserver/Authentication/AuthCodes.cpp
@@ -1,10 +1,5 @@
 /*
  * Copyright (C) 2008-2013 TrinityCore <http://www.trinitycore.org/>
- * Copyright (C) 2013 Project Cerberus <http://www.erabattle.ru/>
- *
- * This program is not free software; you can not redistribute it and/or modify it.
- *
- * This program is distributed only by <http://www.erabattle.ru/>!
  */
 
 #include "AuthCodes.h"
